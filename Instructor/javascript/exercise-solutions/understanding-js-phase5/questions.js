fba2df742dcc2eafe610dc692052d558dcc2115b
training-speclane <training@speclane.com>
2023-07-08 10:34:50 -0400

    Phase 5 - set 1

---
+++ Instructor/javascript/exercise-solutions/understanding-js-phase5/questions.js
@@ -0,0 +1,126 @@
+/**********************************************************************************************
+ * This file contains solutions to phase 5 of Javascript practice questions
+ **********************************************************************************************/
+
+
+/** 1) 
+ * Create a function that takes a number as an argument. 
+ * Add up all the numbers from 1 to the number you passed to the function. For example, 
+ * if the input is 4 then your function should return 10 because 1 + 2 + 3 + 4 = 10.
+ * addUp(4) ➞ 10
+   addUp(13) ➞ 91
+   addUp(600) ➞ 180300
+ */
+
+function addUpTo(lastNumber) {
+   let sum = 0;  // intialize a sum
+   for(let i =1; i <= lastNumber ; i++ ){ //loop through all numbers starting from 1
+      //sum += i;
+      sum = sum + i; // add the current value to the sum
+   }
+   return sum;  //return the sum after the loop has completely gone though each number.
+}
+
+
+/** 2)
+ * 
+ * This challenge will help you interpret mathematical relationships both algebraically and geometrically.
+
+      Matchstick Houses, Steps 1, 2 and 3
+
+      Create a function that takes a number (step) as an argument and returns the number of matchsticks in that step. See step 1, 2 and 3 in the image above.
+
+      Examples
+      matchHouses(1) ➞ 6
+
+      matchHouses(4) ➞ 21
+
+      matchHouses(87) ➞ 436
+      Notes
+      Step 0 returns 0 matchsticks.
+      The input (step) will always be a non-negative integer.
+      Think of the input (step) as the total number of houses that have been connected together.
+ * 
+ */
+
+      function matchHouses(numberOfHouses) {
+         // 1 house = 6 sticks
+         // 2 houses =  6 + (6-1)
+         // 3 houses =  6 + 5 + 5
+         // 4 houses =  6 + 5 + 5 + 5
+         if(numberOfHouses === 0){
+            return 0;
+         }
+         return (5*numberOfHouses) + 1;
+      }
+
+
+
+      /** 3)
+       * Create a function that takes an array of numbers and return both the minimum and maximum numbers, in that order.
+
+         Examples
+         minMax([1, 2, 3, 4, 5]) ➞ [1, 5]
+
+         minMax([2334454, 5]) ➞ [5, 2334454]
+
+         minMax([1]) ➞ [1, 1]
+         Notes
+         All test arrays will have at least one element and are valid.
+       * 
+       */
+
+
+       function minMax(array) {
+
+         //check to see if there are any elements in the array
+         if(array.length === 0){
+            return [];
+         }
+         let min = array[0];
+         let max = array[0];
+         //Loop through the array and find the min and max 
+         for(let i=1; i<array.length; i++){
+            //determine min number
+            if(array[i] < min){
+               min = array[i];
+            }
+            //determine max number
+            if(array[i] >  max){
+               max = array[i];
+            }
+         }
+         //return min and max numbers as array, starting with the min as first element
+         return [min, max]
+       }
+
+
+
+       /**
+        * You will be given an array of drinks, with each drink being an object with two properties: name and price. Create a function that has the drinks array as an argument and return the drinks objects sorted by price in ascending order.
+
+            Assume that the following array of drink objects needs to be sorted:
+
+            drinks = [
+            {name: "lemonade", price: 50},
+            {name: "lime", price: 10}
+            ]
+            The output of the sorted drinks object will be:
+
+            Examples
+            sortDrinkByPrice(drinks) ➞ [{name: "lime", price: 10}, {name: "lemonade", price:50}]
+        * 
+        */
+
+
+            function sortDrinkByPrice(drinks) {
+
+               //use the Javascript compare function to sort drinks by price
+               //When using compare function, sorting from a-b ==> ascending and b-a means descending
+
+               const sortedDrinks =  drinks.sort((a, b) => a.price - b.price); // we want to sort price from small - large(a.price - b.price)
+              
+               console.log(sortedDrinks);
+               return sortedDrinks;
+
+            }
