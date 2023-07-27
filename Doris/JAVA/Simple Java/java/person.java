933ac532e559ab54b71c9d3f00052e194eb7c948
Dunamite1 <dandydoris4@gmail.com>
2023-07-28 01:29:14 +0200

    springboot

---
+++ Doris/JAVA/Simple Java/java/person.java
@@ -0,0 +1,34 @@
+package java;
+
+public class person {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private double height;
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public double getHeight() {
+        return height;
+    }
+    public void setHeight(double height) {
+        this.height = height;
+    }
+    
+    
+}
