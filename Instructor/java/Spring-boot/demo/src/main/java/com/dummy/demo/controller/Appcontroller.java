a9bc84d135c93b4ed6de19282e03d70023d28252
training-speclane <training@speclane.com>
2023-07-27 21:12:10 -0400

    Connecting React app to Springboot

--- Instructor/java/Spring-boot/demo/src/main/java/com/dummy/demo/controller/Appcontroller.java
+++ Instructor/java/Spring-boot/demo/src/main/java/com/dummy/demo/controller/Appcontroller.java
@@ -1,7 +1,10 @@
 package com.dummy.demo.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.dummy.demo.entity.User;
 
 @RestController
 public class Appcontroller {
@@ -10,10 +13,28 @@ public class Appcontroller {
 
     @GetMapping("/get-app")
   public String getApp () {
-
-    
     return "Hello";
   }
+
+
+
+  
+  @CrossOrigin(origins = "http://localhost:3000/")
+  @GetMapping("/get-user-info")
+  public User getUserInfo () {
+    User user = new User();
+    user.setCity("NYC");
+    user.setCountry("USA");
+    user.setFirstName("John");
+    user.setLastName("Doe");
+    user.setStreetName("5th avn");
+    user.setDob("7/27/2032");
+  
+    return user;
+    
+  }
+ 
+
 
 
 }
---
+++ Instructor/java/Spring-boot/demo/src/main/java/com/dummy/demo/entity/User.java
@@ -0,0 +1,55 @@
+package com.dummy.demo.entity;
+
+public class User {
+
+
+    private String firstName = "James";
+    private String lastName = "Paul";
+    private String dob = "12/12/1985";
+    private String country;
+    private String city;
+    private String streetName;
+
+
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getDob() {
+        return dob;
+    }
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+    public String getCountry() {
+        return country;
+    }
+    public void setCountry(String country) {
+        this.country = country;
+    }
+    public String getCity() {
+        return city;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public String getStreetName() {
+        return streetName;
+    }
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+
+    
+}
