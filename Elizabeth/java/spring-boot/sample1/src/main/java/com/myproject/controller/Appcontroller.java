02f8bb912ad9668f1a5103ccf89ce4cf550b4e60
elizabeth-123456 <elizabethameg@gmail.com>
2023-09-13 00:33:50 -0400

    add

--- Elizabeth/java/spring-boot/sample1/src/main/java/com/myproject/controller/Appcontroller.java
+++ Elizabeth/java/spring-boot/sample1/src/main/java/com/myproject/controller/Appcontroller.java
@@ -1,7 +1,14 @@
+
  
  @RestController //Request and response should be JSON
  public class Appcontroller {
+ 
 
-    
+    @GetMapping("/test")
+  public String CheckApplication ()  {
+
+     return "This is a string";
+
+  }
     
 }
